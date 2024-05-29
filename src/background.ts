f0c0a75ad70d13427cdb13af8b7501356629632c
Milia lahouazi <mlahouazi@myges.fr>
2024-05-29 14:49:44 +0200

    add dark mode

--- src/background.ts
+++ src/background.ts
@@ -1,28 +1,3 @@
-// function createQuestionMark(): HTMLElement {
-//   const questionMark = document.createElement("div");
-//   questionMark.textContent = "?";
-//   questionMark.classList.add("question-mark");
-//   questionMark.style.left = `${Math.random() * 100}vw`;
-//   questionMark.style.animationDuration = `${Math.random() * 3 + 3}s`;
-//   return questionMark;
-// }
-
-// function addQuestionMarks(container: HTMLElement, count: number) {
-//   for (let i = 0; i < count; i++) {
-//     const questionMark = createQuestionMark();
-//     container.appendChild(questionMark);
-
-//     questionMark.addEventListener("animationend", () => {
-//       questionMark.remove();
-//       container.appendChild(createQuestionMark());
-//     });
-//   }
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const container = document.getElementById("question-mark-container")!;
-//   addQuestionMarks(container, 50);
-// });
 function createQuestionMark(): HTMLElement {
   const questionMark = document.createElement("div");
   questionMark.textContent = "?";
@@ -47,4 +22,7 @@ function addQuestionMarks(container: HTMLElement, count: number) {
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("question-mark-container")!;
   addQuestionMarks(container, 50);
+});
+document.getElementById("mode-toggle").addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
 });
